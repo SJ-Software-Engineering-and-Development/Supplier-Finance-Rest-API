6ff271e65d10a6108ecd543821f479c7bfe510bc
sjpeeris085 <tombraider085@gmail.com>
2021-05-14 17:42:55 +0530

    init_project

---
+++ SupplierFinance/src/test/java/com/hcl/SupplierFinance/SupplierFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.SupplierFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
